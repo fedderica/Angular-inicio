b204827a10c89b3e7351bc98360b2814ea5fb134
fedderica <fedderica.bartoli@gmail.com>
2023-04-17 13:19:34 -0300

    base.

---
+++ Clases.js
@@ -0,0 +1,12 @@
+Clases:
+Moldes que le dan vida a los objetos
+Instanciamos objetos basados en clases
+100% orientado al uso de clases. 
+
+Padre>hijo>Herencia.Clases
+Interfaces. > el programador Implementará la interfaz
+Inyección de dependencias> importar componentes o librerías. 
+
+RXJS = tecnologia para manejar flujo de eventos 
+(ej dar click a algun boton)
+
